05c72b41a9361c53dd5c44b88b05f863d7ced5b3
Nemanja Manojlovic <manojlovic911@hotmail.com>
2023-07-11 20:13:17 +0200

    Update base style, and presentation layer of the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Providers } from './Providers';
+import React from "react";
+import "./App.css";
+import { Providers } from "./Providers";
+import Heading from "./components/Heading";
+import Card from "./components/Card";
+import BookmarksTab from "./components/BookmarksTab";
+import SearchTab from "./components/SearchTab";
+import SearchForm from "./components/SearchForm";
+import Image from "./components/Image";
+import SettingsTab from "./components/SettingsTab";
 
 function App() {
   return (
     <Providers>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://prettycoffee.github.io/startpage"
-            target="_blank"
-            rel="noopener noreferrer"
-            >
-            Read the docs
-          </a>
-        </header>
-      </div>
+      <Card>
+        <div className="image-wrapper">
+          <Image
+            alt=""
+            className="image"
+            src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZzA0YWc5czVob3d4cjFvcnc0MXV1aG01d3Bqbm94ZWZuNjNwdjM5OCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/84SFZf1BKgzeny1WxQ/giphy.gif"
+          />
+        </div>
+        <main>
+          <Heading>Happy Hacking!</Heading>
+          <SearchTab>
+            <SearchForm />
+          </SearchTab>
+          {/* <!-- Bookmarks will get inserted here! --> */}
+          <BookmarksTab />
+        </main>
+        <SettingsTab />
+      </Card>
     </Providers>
   );
 }
